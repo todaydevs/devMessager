6fb6e4da15645f0400731063842e9101d4c9a573
Abass Ben Cheik <cheikabassben@gmail.com>
2021-06-14 23:05:44 +0000

    Feat: Add join and chat components

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import Chat from "./component/chat";
+import Chat from "./component/Chat/Chat";
+import Join from "./component/Join/Join";
 
 const App = () => {
   return (
     <Router>
      <Route path="/" exact component={Chat} />
+     <Route path="/join" component={Join} />
     </Router>
     )
 }
--- client/src/component/join.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Join = () => {
-  return (
-    <h1>Dev Messager</h1>
-    )
-}
-
-export default Join;
